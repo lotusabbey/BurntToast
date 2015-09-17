5d3ef0df4891ea0019a3d80682f246f8eddf7064
Joshua King <windosbk+github@gmail.com>
2015-09-17 16:08:19 +1200

    filename set and loader updated (only if win10)

--- loader.psm1
+++ loader.psm1
@@ -1,3 +1,8 @@
 ﻿. $PSScriptRoot\New-BurntToastNotification.ps1
 . $PSScriptRoot\Test-PSDifferentUser.ps1
 . $PSScriptRoot\ValidationScripts.ps1
+
+if ([Environment]::OSVersion.Version.Major -ge 10)
+{
+    . $PSScriptRoot\AdaptiveToast_Classes.ps1
+}
